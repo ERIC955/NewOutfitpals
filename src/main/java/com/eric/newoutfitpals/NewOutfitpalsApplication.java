eac86092b37e793e42cf5a816089ef8b0add5191
eric <26416387.re@gmail.com>
2023-12-21 12:31:19 +0800

    建資料

---
+++ src/main/java/com/eric/newoutfitpals/NewOutfitpalsApplication.java
@@ -0,0 +1,13 @@
+package com.eric.newoutfitpals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewOutfitpalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewOutfitpalsApplication.class, args);
+	}
+
+}
